24d4467d4d7f2251d233fe41b098967f3e52e662
Sameer singh <Sameersinghmodanwal@gmail.com>
2025-01-06 04:16:25 +0530

    Update recurtion_logic.py
    Add some other example

--- recurtion_logic.py
+++ recurtion_logic.py
@@ -16,3 +16,15 @@ def fact(n):
         return n*fact(n-1)
 print(fact(int(input("Enter a number : "))))
 #Example input 5 then output 120
+#how to call one function in another function by using one function
+#cheak the number is odd or even
+def even_recurstion(x):
+    if x == 0:
+        return 1
+    else:
+        return odd_recurstion(x-1)
+def odd_recurstion(x):
+    return not even_recurstion(x)
+print(even_recurstion(10))
+print(odd_recurstion(9))
+#output Ture , Ture
